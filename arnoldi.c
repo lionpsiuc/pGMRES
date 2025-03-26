72c3353fa9c2c176b7c8721d06e7bee3b8c492e0
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-03-26 11:47:00 +0000

    Make orthonormal basis and Hessenberg matrices global to ensure they remain in scope

--- arnoldi.c
+++ arnoldi.c
@@ -14,6 +14,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double **V_global = NULL;
+double **H_global = NULL;
+
 /**
  * @brief Allocates memory for a matrix.
  *
@@ -99,11 +102,17 @@ void mvm(double **A, double *v, double *result, int n) {
  * @param[in] m Number of Arnoldi iterations.
  */
 void arnoldi(double **A, double *v1, int m) {
-  int n = 10; // Matrix size
+  int n = 10;             // Matrix size
+  if (V_global != NULL) { // Free memory if it was allocated before
+    free_matrix(V_global, n);
+  }
+  if (H_global != NULL) { // Free memory if it was allocated before
+    free_matrix(H_global, m);
+  }
 
   // Allocate memory for orthonormal basis matrix and Hessenberg matrix
-  double **V_global = allocate_matrix(n, m + 1);
-  double **H_global = allocate_matrix(m + 1, m);
+  V_global = allocate_matrix(n, m + 1);
+  H_global = allocate_matrix(m + 1, m);
 
   // Allocate memory for intermediate results as per the pseudocode
   double *w = (double *)malloc(n * sizeof(double));
